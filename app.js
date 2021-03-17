ce9e1e442808422e0e6b7ef6bf980533a4d24e90
o0p <rus@gmx.com>
2021-03-17 09:55:21 +0000

    Add localStorage condition

--- app.js
+++ app.js
@@ -2,6 +2,12 @@ const form = document.getElementById('form');
 const input = document.getElementById('input');
 const todosUL = document.getElementById('todos');
 
+const todos = JSON.parse(localStorage.getItem('todos'));
+
+if (todos) {
+    todos.forEach(todo => addTodo(todo))
+};
+
 form.addEventListener('submit', (e) => {
     e.preventDefault()
     addTodo();
@@ -32,3 +38,4 @@ function addTodo(todo) {
         input.value = '';
     };
 };
+
